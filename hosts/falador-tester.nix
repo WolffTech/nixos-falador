eca05ec0df36cbfbd12909360d82d40568b03ac0
Nick Wolff <nick@wolff.tech>
2025-05-26 16:14:20 -0400

    tester: set networking
    Set the following settings on tester machine
    - Enable Firewall
    - Set static IP
    - Allow SSH port

--- hosts/falador-tester.nix
+++ hosts/falador-tester.nix
@@ -1,5 +1,20 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   networking.hostName = "Falador-Tester";
+  networking.firewall.enable = lib.mkForce true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedUDPPorts = [ ];
+
+  networking.interfaces.ens18 = {
+    ipv4.addresses = [
+      {
+        address = "172.16.1.29";
+        prefixLength = 24;
+      }
+    ];
+  };
+
+  networking.defaultGateway = "172.16.1.1";
+  networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
 }
