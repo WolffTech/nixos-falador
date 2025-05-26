0869e55b166844f6a06f7808a1340b15fc660329
Nick Wolff <nick@wolff.tech>
2025-05-26 05:01:19 -0400

    tester: update home files

--- home/falador-tester.nix
+++ home/falador-tester.nix
@@ -1,10 +1,23 @@
 { config, pkgs, dotfiles, ... }:
 
 {
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  programs.zsh.enable = true;
+  programs.git.enable = true;
+
   home.packages = with pkgs; [
+    nvim
+    lazygit
   ];
 
   home.file = {
     ".zshrc".source = "{dotfiles}/dot_zshrc";
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
   };
 }
