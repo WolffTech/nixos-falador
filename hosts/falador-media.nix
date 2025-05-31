70c0c6342d15c6657435a01de52c832e3872eea4
Nick Wolff <nick@wolff.tech>
2025-05-31 19:10:27 -0400

    media: attach network storage
    To keep the username and password secret, created a secrets.nix file
    that will be stored on /etc/nixos/* on the machine. This file, for
    obvious reasons, will be created on the machine and ignored by git so
    that these creds stay private. Will find a better way to do this later.

--- hosts/falador-media.nix
+++ hosts/falador-media.nix
@@ -1,5 +1,8 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  secrets = import /etc/nixos/secrets.nix;
+in
 {
   # hostname
   networking.hostName = "Falador-Media";
@@ -9,4 +12,25 @@
     plex
     jellyfin
   ];
+
+  # mount smb share
+  boot.kernelModules = [ "cifs" ];
+
+  fileSystems."/mnt/ServerStorage" = {
+    device  = "//172.16.1.2/ServerStorage";
+    fsType  = "cifs";
+    options = [
+      "credentials=/etc/samba/creds"
+      "uid=1000"
+      "gid=100"
+      "file_mode=0644"
+      "dir_mode=0755"
+    ];
+  };
+
+  environment.etc."samba/creds".text = lib.concatStringsSep "\n" [
+    "username=${secrets.username}"
+    "password=${secrets.password}"
+  ];
+  environment.etc."samba/creds".mode = "0600";
 }
