ae9873355061872cc1844c58fe6b9a7dd7938b92
Nick Wolff <nick@wolff.tech>
2025-06-01 19:40:18 -0400

    media: set networking settings

--- hosts/falador-media.nix
+++ hosts/falador-media.nix
@@ -7,6 +7,24 @@ in
   # hostname
   networking.hostName = "Falador-Media";
 
+  # networking
+  networking.firewall.enable = lib.mkForce true;
+  networking.firewall.allowPing = lib.mkForce true;
+  networking.firewall.allowedTCPPorts = [ 22 139 445 ];
+  networking.firewall.allowedUDPPorts = [ 137 138 ];
+
+  networking.interfaces.ens18 = {
+    ipv4.addresses = [
+      {
+        address = "172.16.1.24";
+        prefixLength = 24;
+      }
+    ];
+  };
+
+  networking.defaultGateway = "172.16.1.1";
+  networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
+
   # extra packages
   environment.systemPackages = with pkgs; [
     cifs-utils
